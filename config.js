14885c13776c74ebadc4af343948b0e39f90f33f
jafarmohd2 <jafar.mohd2@gmail.com>
2021-11-20 11:45:45 +0530

    Integrate Pinata

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
-export const nftaddress = "0xf8329ca5B5F5287DE16d3408def7c01640Bd3225"
+export const nftaddress = "0xf8329ca5B5F5287DE16d3408def7c01640Bd3225";
+export const pinata_api_key = "e23c3d148c0d6c7d1e7a";
+export const pinata_secret_api_key = "53bdfb8fede7d88cbbcdece7f4b72871efd5eb1bd0a6c96c39cb3efbc6b87882";
